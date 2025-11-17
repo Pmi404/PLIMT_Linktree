744460448c8e3cc1b005319dcc7026b9d4e3910b
Pmi404 <multipedrolucas@gmail.com>
2025-11-17 19:56:05 -0300

    big-commit

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/Plimt_light.jpg")
+    img.setAttribute("src", "./assets/logo.jpg")
     img.setAttribute("alt", "Mayk Brito assustado com o sol")
   } else {
-    img.setAttribute("src", "./assets/Plimt_dark.jpg")
+    img.setAttribute("src", "./assets/logo.jpg")
     img.setAttribute("alt", "Mayk Brito, modo escuro")
   }
 }
